8bd14107fc3e4a1d72029fe3cea9682a8a3a2b41
rueterscooter <32781947+rueterscooter@users.noreply.github.com>
2019-10-29 18:39:23 -0700

    Added spin attack.
    Still to be done: visual/audio cue, increase damage for spin attack, network capability.
    
    Also needs a solution to trapping mobs against wall, as the sword no longer disappears after making contact with the enemy and kills them instantly. I recommend a pushback on the player, possibly a stab animation/state, and invincibility frames for the enemy.

--- items/sword.gd
+++ items/sword.gd
@@ -36,11 +36,37 @@ remote func set_pos(p_pos):
 
 sync func delete():
 	get_parent().state = "default"
+	get_parent().spinAtk = false
 	queue_free()
 
 func _physics_process(delta):
-	delete_on_hit = true
-	if get_parent().has_method("state_hold"):
+	if get_parent().has_method("state_hold") and get_parent().state != "spin":
 		get_parent().state = "hold"
+		if get_parent().holdTimer.is_stopped() and !get_parent().spinAtk:
+			get_parent().holdTimer.start()
 	if !Input.is_action_pressed(input):
-		destroy(null)
+		
+		# Spin attack
+		if get_parent().has_method("state_spin") and get_parent().spinAtk and get_parent().state != "spin":
+			get_parent().state = "spin"
+			anim.play("spin")
+			match get_parent().spritedir:
+				"Left":
+					anim.advance(0.2)
+				"Right":
+					anim.advance(0.2)
+					scale.x = -1
+				"Up":
+					anim.advance(0.08)
+				"Down":
+					anim.advance(0.3)
+			get_parent().anim.connect("animation_finished", self, "destroy")
+			get_parent().anim.connect("animation_changed", self, "destroy")
+			sfx.play(load(str("res://items/sword_swing",int(rand_range(1,5)),".wav"))) # get beter sfx
+		else:
+			if get_parent().get("holdTimer"):
+				get_parent().holdTimer.stop()
+				if not get_parent().spinAtk:
+					destroy(null)
+			else:
+				destroy(null)
