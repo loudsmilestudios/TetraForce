b72290d9f60f93efcb8cb21795222640e3cf8cb2
Nodding <32770553+Nodding@users.noreply.github.com>
2019-10-31 14:39:10 -0400

    They now shoot in the direction you specify

--- arrow_shooter.gd
+++ arrow_shooter.gd
@@ -1,17 +1,17 @@
 extends Entity
 
 export (float) var timer
-
+export (String) var direction
 var movetimer = 0
 
 func _ready():
-	var direction  = spritedir
-
+	spritedir = direction
 
 func _physics_process(delta):
 	var movetimer_length = timer
 	if movetimer >= 0:
 		movetimer -= 1
 	else:
+		
 		use_item("res://items/arrow.tscn", "A")
 		movetimer = movetimer_length
