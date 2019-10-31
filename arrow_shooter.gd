d62b28325272e743f1602fec5b86559121bc1d43
Nodding <32770553+Nodding@users.noreply.github.com>
2019-10-31 14:45:24 -0400

    No longer spams phys process

--- arrow_shooter.gd
+++ arrow_shooter.gd
@@ -8,10 +8,9 @@ func _ready():
 	spritedir = direction
 
 func _physics_process(delta):
-	var movetimer_length = timer
 	if movetimer >= 0:
 		movetimer -= 1
 	else:
 		
 		use_item("res://items/arrow.tscn", "A")
-		movetimer = movetimer_length
+		movetimer = timer
