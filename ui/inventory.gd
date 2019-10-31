84c7931bfaafce0e63effab70bd412dba602604f
gboydhub <eponickstream@gmail.com>
2019-10-31 16:15:30 -0400

    Cursor movement

--- ui/inventory.gd
+++ ui/inventory.gd
@@ -5,6 +5,9 @@ var MAX_SELECT = 35
 
 var has_item = []
 var item_list = []
+var cursor_position = 0
+
+var cursor_node
 
 func _ready():
 	for i in range(MAX_SELECT+1):
@@ -22,8 +25,31 @@ func _ready():
 			if !has_item[item_counter]:
 				item.visible = false
 			item_counter += 1
+			
+	cursor_node = get_child(0).get_child(0).get_child(0).get_child(0)
 	pass
 
+func update_cursor_loc():
+	var column_selected = cursor_position%6
+	var row_selected = floor(cursor_position/6)
+	var item_selected = item_list[row_selected][column_selected]
+	var parent_pos = item_selected.get_parent()
+	var new_pos = item_selected.rect_position + parent_pos.rect_position
+	new_pos.x -= 2
+	new_pos.y -= 2
+	cursor_node.rect_position = new_pos
+	
 func _process(delta):
 	if Input.is_action_just_pressed("RIGHT"):
-		print_debug("pressed right")
+		cursor_position = min(cursor_position + 1, MAX_SELECT)
+		update_cursor_loc()
+	elif Input.is_action_just_pressed("LEFT"):
+		cursor_position = max(cursor_position - 1, 0)
+		update_cursor_loc()
+	elif Input.is_action_just_pressed("UP"):
+		cursor_position = max(cursor_position - 6, 0)
+		update_cursor_loc()
+	elif Input.is_action_just_pressed("DOWN"):
+		cursor_position = min(cursor_position + 6, MAX_SELECT)
+		update_cursor_loc()
+	pass
