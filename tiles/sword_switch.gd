2f74905c65d542f486c3be27a12d5b4ebc012c08
RedstoneParadox <spinjitzu00@gmail.com>
2019-11-01 12:33:11 -0700

    Fixed some networking and sprite issues && added mode enum

--- tiles/sword_switch.gd
+++ tiles/sword_switch.gd
@@ -3,6 +3,8 @@ extends Node2D
 signal on_activate
 signal on_deactivate
 
+export(int, "one_shot", "toggle", "time_out") var mode: int = 0
+
 export(bool) var one_shot: bool = false
 
 var activated: bool = false
@@ -17,13 +19,14 @@ func _physics_process(delta):
 func _on_HitBox_area_entered(area: Area2D):
 	var sprite = $Sprite
 	
-	if one_shot:
+	if mode == 0:
 		if !activated:
 			activated = true
+			sprite.frame_coords.x = 1
 			emit_signal("on_activate")
 			update_remote("enable_remote")
 			
-	else:
+	elif mode == 1:
 		
 		if !on_cooldown:
 			activated = !activated
@@ -45,7 +48,7 @@ func _on_CooldownTimer_timeout():
 	on_cooldown = false
 	
 	for peer in network.map_peers:
-		rset_id(peer, "on_cooldown", false)
+		rpc_id(peer, "remote_cooldown_timeout")
 
 func update_remote(function: String):
 	for peer in network.map_peers:
@@ -54,9 +57,12 @@ func update_remote(function: String):
 remote func enable_remote():
 	if !on_cooldown:
 		$Sprite.frame_coords.x = 1
-		emit_signal("on_deactivate")
+		emit_signal("on_activate")
 
 remote func disable_remote():
 	if !on_cooldown and !one_shot:
 		$Sprite.frame_coords.x = 0
 		emit_signal("on_deactivate")
+
+remote func remote_cooldown_timeout():
+	on_cooldown = false
