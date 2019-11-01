ad310107755a1dce4fdaf17a53629de6ea19579b
RedstoneParadox <spinjitzu00@gmail.com>
2019-10-31 22:03:15 -0700

    Add prototype sword switch

---
+++ tiles/sword_switch.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+signal on_activate
+
+export(bool) var one_shot: bool = false
+
+var activated: bool = false
+var on_cooldown: bool = false
+
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_HitBox_area_entered(area):
+	var sprite = $Sprite
+	
+	if one_shot:
+		if !activated:
+			activated = true
+			emit_signal("on_activate")
+	else:
+		
+		if !on_cooldown:
+			activated = !activated
+			
+			if (activated):
+				sprite.frame_coords.x = 1
+				emit_signal("on_activate")
+			else:
+				sprite.frame_coords.x = 0
+			
+			$CooldownTimer.start()
+			on_cooldown = true
+
+func _on_CooldownTimer_timeout():
+	on_cooldown = false
