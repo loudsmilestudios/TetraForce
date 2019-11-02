97a47a7a477cc0bea7fb5335763c677099438ea8
Nodding <32770553+Nodding@users.noreply.github.com>
2019-11-02 19:08:27 -0400

    Small fixes
    Added the networking lines, disabled the collision box, and fixed a small graphical bug.

--- enemies/arrowtrap/arrow_trap.gd
+++ enemies/arrowtrap/arrow_trap.gd
@@ -11,9 +11,12 @@ func _ready():
 	spritedir = direction
 	if ["Left", "Up", "Right", "Down"].has(spritedir):
 		$AnimatedSprite.set_animation(spritedir.to_lower())
+	hitbox.queue_free()
 func _physics_process(delta):
 	if movetimer >= 0:
 		movetimer -= 1
 	else:
 		use_item("res://items/arrow.tscn", "A")
+		for peer in network.map_peers:
+			rpc_id(peer, "use_item", "res://items/arrow.tscn", "A")
 		movetimer = timer
