bbd4b38a033a1b1aab2861bca9496423705be5f9
RedstoneParadox <spinjitzu00@gmail.com>
2019-11-01 18:21:34 -0700

    Cleaned up and documented code.

--- engine/switch.gd
+++ engine/switch.gd
@@ -5,14 +5,15 @@ signal on_activate
 signal on_deactivate
 
 export(int, "one_shot", "toggle", "time_out") var mode: int = 0
-export(float) var cooldown: float = 0.1
+export(float) var cooldown: float = 5
 
 var activated: bool = false
-var on_cooldown: bool = false
 
+# Called in subclasses to update the state on the client or the server.
 func update_state():
 	var new_state = !activated
 	
+	# Call the state change function locally then remotely.
 	change_state(new_state)
 	for peer in network.map_peers:
 		rpc_id(peer, "update_remote_state", new_state)
@@ -22,41 +23,39 @@ remote func update_remote_state(new_state: bool):
 
 func change_state(new_state: bool):
 	
+	# Oneshot mode
 	if mode == 0:
 		if !activated:
 			activated = true
 			emit_signal("on_activate")
-		
+	
+	# Toggle mode
 	elif mode == 1:
-		
-		if !on_cooldown:
-			activated = new_state
-			
-			if activated:
-				emit_signal("on_activate")
-			else:
-				emit_signal("on_deactivate")
-			
-			on_cooldown = true
-			$CooldownTimer.start(cooldown)
-		
+		activated = new_state
+		# Test the new state then emit the appropriate signal.
+		if activated:
+			emit_signal("on_activate")
+		else:
+			emit_signal("on_deactivate")
+	
+	# Timeout mode
 	elif mode == 2:
 		
+		# If not activated, activate then start the cooldown timer.
 		if !activated:
 			activated = true
-			on_cooldown = true
 			emit_signal("on_activate")
 			$CooldownTimer.start(cooldown)
 	
 	_update_sprite()
 
+# Called when the sprite needs to be changed to reflect an updated state.
+# Override this in subclasses.
 func _update_sprite():
 	pass
 
+# Deactivate the switch once the TimeoutTimer is finished.
 func finish_cooldown():
-	on_cooldown = false
-	
-	if mode == 2:
-		activated = false
-		emit_signal("on_deactivate")
-		_update_sprite()
+	activated = false
+	emit_signal("on_deactivate")
+	_update_sprite()
