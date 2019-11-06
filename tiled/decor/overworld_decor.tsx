3fbe43a9082d049b7938448a5636fb48dd52513c
TheRetroDragon <andrew@retro-dragon.com>
2019-11-05 20:45:00 -0700

    First Tiled Implement .

---
+++ tiled/decor/overworld_decor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="overworld_decor" tilewidth="16" tileheight="16" tilecount="5" columns="5">
+ <image source="../images/decor/overworld_decor.png" width="80" height="16"/>
+</tileset>
---
+++ tiled/objects/overworld_objects.tsx
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="overworld_objects" tilewidth="16" tileheight="16" tilecount="50" columns="5">
+ <image source="../images/objects/overworld_objects.png" width="80" height="160"/>
+ <terraintypes>
+  <terrain name="Grass" tile="11"/>
+  <terrain name="Dead Grass" tile="11"/>
+  <terrain name="Hole" tile="46"/>
+ </terraintypes>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="5" terrain=",,,0"/>
+ <tile id="6" terrain=",,0,0"/>
+ <tile id="7" terrain=",,0,"/>
+ <tile id="8" terrain="0,0,0,"/>
+ <tile id="9" terrain="0,0,,0"/>
+ <tile id="10" terrain=",0,,0"/>
+ <tile id="11" terrain="0,0,0,0"/>
+ <tile id="12" terrain="0,,0,"/>
+ <tile id="13" terrain="0,,0,0"/>
+ <tile id="14" terrain=",0,0,0"/>
+ <tile id="15" terrain=",0,,"/>
+ <tile id="16" terrain="0,0,,"/>
+ <tile id="17" terrain="0,,,"/>
+ <tile id="20" terrain=",,,1"/>
+ <tile id="21" terrain=",,1,1"/>
+ <tile id="22" terrain=",,1,"/>
+ <tile id="23" terrain="1,1,1,"/>
+ <tile id="24" terrain="1,1,,1"/>
+ <tile id="25" terrain=",1,,1"/>
+ <tile id="26" terrain="1,1,1,1"/>
+ <tile id="27" terrain="1,,1,"/>
+ <tile id="28" terrain="1,,1,1"/>
+ <tile id="29" terrain=",1,1,1"/>
+ <tile id="30" terrain=",1,,"/>
+ <tile id="31" terrain="1,1,,"/>
+ <tile id="32" terrain="1,,,"/>
+ <tile id="35" terrain="2,2,2,"/>
+ <tile id="36" terrain="2,2,,2"/>
+ <tile id="37" terrain=",,,2"/>
+ <tile id="38" terrain=",,2,2"/>
+ <tile id="39" terrain=",,2,"/>
+ <tile id="40" terrain="2,,2,2"/>
+ <tile id="41" terrain=",2,2,2"/>
+ <tile id="42" terrain=",2,,2"/>
+ <tile id="43" terrain="2,2,2,2"/>
+ <tile id="44" terrain="2,,2,"/>
+ <tile id="47" terrain=",2,,"/>
+ <tile id="48" terrain="2,2,,"/>
+ <tile id="49" terrain="2,,,"/>
+</tileset>
