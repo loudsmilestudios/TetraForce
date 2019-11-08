beb2485938fe5be71e0bce3536888c4e3e60a502
J. Frankenstein <35669696+lelanophilus@users.noreply.github.com>
2019-11-08 09:30:02 -0800

    Update network Room class to keep track of enemies and players (#60)

--- engine/network.gd
+++ engine/network.gd
@@ -151,32 +151,41 @@ class Room :
 	#var map 
 	var tile_rect = Rect2(0, 0, 16, 9)
 	var entities = []
-	var enemies = [] 
+	var enemies = {}
+	var players = {}
 	
 	signal player_entered()
+	signal first_player_entered()
 	signal player_exited()
+	signal last_player_exited()
 	signal enemies_defeated()
 	signal empty()
 	
 	func add_entity(entity):
 		entities.append(entity)
 		
-		if entity.is_in_group("enemy"):
-			enemies.append(entity)
+		if entity.get("TYPE") == "ENEMY":
+			enemies[entity.get_instance_id()] = true
 		
-		if entity.is_in_group("player"):
+		if entity.get("TYPE") == "PLAYER":
+			if players.size() == 0:
+				emit_signal("first_player_entered")
+			players[entity.get_instance_id()] = true
 			emit_signal("player_entered")
-	
+
 	func remove_entity(entity):
 		entities.erase(entity)
 		
-		if entity.is_in_group("enemy"):
-			enemies.erase(entity)
+		if entity.get("TYPE") == "ENEMY":
+			enemies.erase(entity.get_instance_id())
 			
 			if enemies.empty():
 				emit_signal("enemies_defeated")
 		
-		if entity.is_in_group("player"):
+		if entity.get("TYPE") == "PLAYER":
+			players.erase(entity.get_instance_id())
+			if players.size() == 0:
+				emit_signal("last_player_exited")
 			emit_signal("player_exited")
 		
 		if entities.empty():
