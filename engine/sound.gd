f46a2770eeb94c65303c064432c69bde9c6c77e7
acurteman <alexander.curteman@gmail.com>
2019-11-08 15:47:54 -0800

    Created options menu, and basic audio options

--- engine/sound.gd
+++ engine/sound.gd
@@ -4,6 +4,7 @@ func _ready():
 	pause_mode = PAUSE_MODE_PROCESS
 
 func play(sound, volume_modifier=0):
+	print("Audio played")
 	var volume = linear2db(.05 + volume_modifier)
 	var sfx = AudioStreamPlayer.new()
 	get_tree().get_root().add_child(sfx)
---
+++ ui/options/Audio.gd
@@ -0,0 +1,22 @@
+extends Tabs
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$master_pct.text = str($master_slider.value * 100) + "%"
+	$effect_pct.text = str($effects_slider.value *100) + "%"
+	$music_pct.text = str($music_slider.value * 100) + "%"
+
+func _on_master_slider_value_changed(value):
+	$master_pct.text = str($master_slider.value * 100) + "%"
+
+
+func _on_effects_slider_value_changed(value):
+	$effect_pct.text = str($effects_slider.value *100) + "%"
+
+
+func _on_music_slider_value_changed(value):
+	$music_pct.text = str($music_slider.value * 100) + "%"
