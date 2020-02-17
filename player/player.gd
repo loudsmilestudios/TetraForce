d61a8209ab296934800d3d0545f319eb5c22d680
RedstoneParadox <spinjitzu00@gmail.com>
2020-02-16 16:06:04 -0800

    Restored the Overworld Demo map

--- player/player.gd
+++ player/player.gd
@@ -21,10 +21,10 @@ func _ready() -> void:
 		var hud = get_parent().get_node("HUD")
 		hud.initialize()
 		
-		position = get_parent().get_node(global.next_entrance).position
-		var offset = get_parent().get_node(global.next_entrance).player_position
-		# position = Vector2(1104, 1088)
-		# var offset = "right"
+		# position = get_parent().get_node(global.next_entrance).position
+		# var offset = get_parent().get_node(global.next_entrance).player_position
+		position = Vector2(1104, 1088)
+		var offset = "right"
 		match offset:
 			"up":
 				position.y -= 16
