b388ed04fea5a8632022a54638f7f6697042142e
fornclake <josephs.lesko@gmail.com>
2020-07-24 02:02:25 -0500

    added block to tiled objects

--- tiled/objects.tsx
+++ tiled/objects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.5" name="objects" tilewidth="16" tileheight="16" tilecount="2" columns="0">
+<tileset version="1.2" tiledversion="1.2.5" name="objects" tilewidth="16" tileheight="16" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <properties>
@@ -15,5 +15,11 @@
    <property name="player_position" value=""/>
   </properties>
   <image width="16" height="16" source="images/objects/exit.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="path" value="res://tiles/block.tscn"/>
+  </properties>
+  <image width="16" height="16" source="images/objects/block.png"/>
  </tile>
 </tileset>
