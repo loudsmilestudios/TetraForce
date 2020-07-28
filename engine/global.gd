84e9b143cdad61950cb0579e97e7a6b41bbbf05c
Rohit <rdg922@gmail.com>
2020-07-27 21:42:15 -0400

    Simple Allow global change_room() to use tscn

--- engine/global.gd
+++ engine/global.gd
@@ -4,14 +4,14 @@ var player
 
 var next_entrance = "a"
 
-func change_map(map, entrance):
+func change_map(map, file_format, entrance):
 	screenfx.play("fadewhite")
 	yield(screenfx, "animation_finished")
 	
 	var old_map = network.current_map
 	var root = old_map.get_parent()
 	
-	var new_map_path = "res://maps/" + map + ".tmx"
+	var new_map_path = "res://maps/" + map + file_format
 	var new_map = load(new_map_path).instance()
 	
 	old_map.queue_free()
--- tiles/exit.gd
+++ tiles/exit.gd
@@ -3,11 +3,11 @@ extends Area2D
 export(String) var map
 export(String) var player_position
 export(String) var entrance
-
+export(String, ".tmx", ".tscn") var file_format = ".tmx"
 func _ready():
 	connect("body_entered", self, "body_entered")
 
 func body_entered(body):
 	if body.is_in_group("player") && body.is_network_master():
 		body.state = "interact"
-		global.change_map(map, entrance)
+		global.change_map(map, file_format, entrance)
