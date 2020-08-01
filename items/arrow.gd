5300b3f1274eeb5288ad1342295b3df004ba60ba
fornclake <josephs.lesko@gmail.com>
2020-07-31 21:16:46 -0500

    arrow network sync fixes, IP entry now includes port by default

--- items/arrow.gd
+++ items/arrow.gd
@@ -3,13 +3,13 @@ extends Item
 const SPEED = 150
 var movedir = Vector2(0,0)
 var shooter
+var received_sync = false
 
 func start():
 	shooter = get_parent()
+	$Hitbox.connect("body_entered", self, "body_entered")
 	add_to_group("projectile")
 	z_index = shooter.z_index - 1
-	get_parent().remove_child(self)
-	shooter.get_parent().add_child(self)
 	position = shooter.position
 	
 	match shooter.spritedir:
@@ -26,21 +26,28 @@ func start():
 			movedir = Vector2.UP
 			rotation_degrees = 270
 	
+	get_parent().remove_child(self)
+	shooter.get_parent().add_child(self)
 	
-	if is_network_master():
-		var network_object = preload("res://engine/network_object.tscn").instance()
-		add_child(network_object)
-		network_object.require_map_host = false
-		network_object.update_properties = {"position":position}
-		
-		$Hitbox.connect("body_entered", self, "body_entered")
-		set_physics_process(true)
+	if !is_network_master():
+		network.peer_call_id(get_network_master(), self, "request_arrow_sync", [network.pid])
+	set_physics_process(true)
+
+func request_arrow_sync(id):
+	network.peer_call_id(id, self, "arrow_sync", [movedir, rotation_degrees])
+
+func arrow_sync(dir, rot):
+	movedir = dir
+	rotation_degrees = rot
+	received_sync = true
 
 func _physics_process(delta):
 	position += movedir * SPEED * delta
 
 func body_entered(body):
-	if body is Entity && body != shooter:
+	if !received_sync && !is_network_master() && body != shooter:
+		queue_free()
+	elif body is Entity && body != shooter:
 		damage(body)
 	elif body != shooter:
 		delete()
