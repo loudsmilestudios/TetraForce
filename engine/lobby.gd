060c5939375b1eb273ab3a80142fac48b505631d
Nodding <32770553+Nodding@users.noreply.github.com>
2020-08-02 14:23:47 -0400

    Added server functionality

--- engine/lobby.gd
+++ engine/lobby.gd
@@ -1,20 +1,25 @@
 
 extends Control
 
-const DEFAULT_PORT = 4564 # some random number, pick your port properly
+const DEFAULT_PORT = 7777 # some random number, pick your port properly
 
 var map = "res://maps/overworld.tmx"
 
+var is_server = false
 #### Network callbacks from SceneTree ####
 
 func create_level():
 	if !get_tree().is_network_server():
 		network.pid = get_tree().get_network_unique_id()
 	network.initialize()
-	var level = load(map).instance()
-	#level.connect("game_finished",self,"_end_game",[],CONNECT_DEFERRED) # connect deferred so we can safely erase it from the callback
-	get_tree().get_root().add_child(level)
-	hide()
+	
+	if(is_server):
+		print("This is a server and will not load the game.")
+	else:
+		var level = load(map).instance()
+		#level.connect("game_finished",self,"_end_game",[],CONNECT_DEFERRED) # connect deferred so we can safely erase it from the callback
+		get_tree().get_root().add_child(level)
+		hide()
 
 # callback from SceneTree
 func _player_connected(id):
@@ -67,7 +72,8 @@ func _set_status(text,isok):
 func _on_host_pressed():
 	var host = NetworkedMultiplayerENet.new()
 	host.set_compression_mode(NetworkedMultiplayerENet.COMPRESS_RANGE_CODER)
-	var err = host.create_server(DEFAULT_PORT, 15) # max: 1 peer, since it's a 2 players game
+	var port = int(get_node("panel/port").get_text())
+	var err = host.create_server(port, 15) # max: 1 peer, since it's a 2 players game
 	if (err!=OK):
 		#is another server running?
 		_set_status("Can't host, address in use.",false)
@@ -76,7 +82,7 @@ func _on_host_pressed():
 	get_tree().set_network_peer(host)
 	get_node("panel/join").set_disabled(true)
 	get_node("panel/host").set_disabled(true)
-	
+	get_node("panel/status_ok").set_text("Server Started")
 	create_level()
 
 func _on_join_pressed():
@@ -85,10 +91,10 @@ func _on_join_pressed():
 	if (not ip.is_valid_ip_address()):
 		_set_status("IP address is invalid",false)
 		return
-	
+	var port = get_node("panel/port").get_text()
 	var host = NetworkedMultiplayerENet.new()
 	host.set_compression_mode(NetworkedMultiplayerENet.COMPRESS_RANGE_CODER)
-	host.create_client(ip,DEFAULT_PORT)
+	host.create_client(ip,int(port))
 	get_tree().set_network_peer(host)
 	
 	_set_status("Connecting..",true)
@@ -103,6 +109,32 @@ func _ready():
 	get_tree().connect("server_disconnected",self,"_server_disconnected")
 	
 	get_tree().set_auto_accept_quit(false)
+	
+	#For server commandline arguments. Searches for ones passed, then tries to set ones that exist.
+	#Puts arguments passed as "--example=value" in a dictionary.
+	var arguments = {}
+	for argument in OS.get_cmdline_args():
+		if argument.find("=") > -1:
+			var key_value = argument.split("=")
+			arguments[key_value[0].lstrip("--")] = key_value[1]
+	#Checks if debug is true and exists as a passed command
+	if("dedicatedserver" in arguments):
+		if ((arguments.get("dedicatedserver")) == "true"):
+			#Sets to true for other function
+			is_server = true
+			#Some cosmedic surgery on the lobby page.
+			get_node("panel/join").hide()
+			get_node("characterselect").hide()
+			get_node("title").set_text("Tetra Force Server")
+			get_node("panel/host").set_text("Start Server")
+	#this overrides the default port of 7777
+	if("port" in arguments):
+		get_node("panel/port").set_text(arguments["port"])
+	#this does nothing yet, but will give verbose debug information to those who enable it
+	if("debug" in arguments):
+		if ((arguments.get("debug")) == "true"):
+			print("DEBUG ENABLED")
+	
 	
 func _notification(n):
 	if (n == MainLoop.NOTIFICATION_WM_QUIT_REQUEST):
