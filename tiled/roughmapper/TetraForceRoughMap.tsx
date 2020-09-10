e17e83e5d1fd1744c5ebf296435b8cee5bff38c5
TheRetroDragon <andrew@retro-dragon.com>
2020-09-09 22:18:25 -0600

    Rough Mapper Implemented
    Added Half Scale tool for building maps - Credit iq.gobo

---
+++ tiled/roughmapper/TetraForceRoughMap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="TetraForceRoughMap" tilewidth="8" tileheight="8" tilecount="36" columns="6">
+ <image source="TetraForceRoughMap.png" width="48" height="48"/>
+</tileset>
