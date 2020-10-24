58e58bba5ce7e5dbf15aa11f2adc1fa3375e6f00
Joseph Manley <j@cloudsumu.com>
2020-10-24 15:24:54 -0400

    Keep track of collision height in Tiled

--- tiled/objects.tsx
+++ tiled/objects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="objects" tilewidth="16" tileheight="16" tilecount="13" columns="0">
+<tileset version="1.2" tiledversion="1.2.1" name="objects" tilewidth="16" tileheight="16" tilecount="13" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <properties>
@@ -9,6 +9,8 @@
  </tile>
  <tile id="1">
   <properties>
+   <property name="collider_height" type="float" value="1"/>
+   <property name="collider_width" type="float" value="1"/>
    <property name="entrance" value=""/>
    <property name="map" value=""/>
    <property name="path" value="res://tiles/exit.tscn"/>
--- tiles/exit.gd
+++ tiles/exit.gd
@@ -3,10 +3,14 @@ extends Area2D
 export(String) var map
 export(String) var player_position
 export(String) var entrance
+export(float) var collider_height = 1
+export(float) var collider_width = 1
 
 func _ready():
 	add_to_group("entrances")
 	connect("body_entered", self, "body_entered")
+	scale = Vector2(collider_width, collider_height)
+	position -= Vector2((collider_width-1)*8, (collider_height-1)*8)
 
 func body_entered(body):
 	if body.is_in_group("player") && body.is_network_master():
