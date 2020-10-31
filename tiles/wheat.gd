e01ed1f1b59293c016a3ab212b880323aec0f1f4
TheRetroDragon <andrew@retro-dragon.com>
2020-10-31 15:23:28 -0600

    Wheat & Cabbage Interactions

---
+++ tiles/wheat.gd
@@ -0,0 +1,30 @@
+extends TileMap
+
+class_name Wheat
+
+var cut_cells = [] setget enter_cut_cells
+
+func _ready():
+	var network_object = preload("res://engine/network_object.tscn").instance()
+	network_object.enter_properties = {"cut_cells":[]}
+	add_child(network_object)
+
+func cut(hitbox):
+	var tile = world_to_map(hitbox.global_position)
+	process_tile(tile)
+	network.peer_call(self, "process_tile", [tile])
+
+func enter_cut_cells(value):
+	cut_cells = value
+	for cell in cut_cells:
+		set_cellv(cell, -1)
+
+func process_tile(tile):
+	if get_cellv(tile) == -1:
+		return
+	cut_cells.append(tile)
+	set_cellv(tile, -1)
+	update_bitmask_region()
+	var wheat_cut = preload("res://effects/wheat_cut.tscn").instance()
+	network.current_map.add_child(wheat_cut)
+	wheat_cut.global_position = map_to_world(tile) + Vector2(8,6)
