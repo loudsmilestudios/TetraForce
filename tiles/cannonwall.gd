bb2da05cd02754387d95472a2298b4b6efdd421d
TheRetroDragon <andrew@retro-dragon.com>
2021-05-28 20:38:04 -0600

    Bug Fixes

--- tiles/cannonwall.gd
+++ tiles/cannonwall.gd
@@ -1,12 +1,11 @@
 extends StaticBody2D
 
 func on_explosion():
-	if network.is_map_host():
-		global.player.camera.on_screen_shake()
-		$AnimationPlayer.play("explosion")
-		sfx.play("explosion")
-		$Sprite.hide()
-	else:
+	global.player.camera.on_screen_shake()
+	$AnimationPlayer.play("explosion")
+	sfx.play("explosion")
+	$Sprite.hide()
+	if !network.is_map_host():
 		network.peer_call_id(network.get_map_host(), self, "on_explosion", [])
 
 
