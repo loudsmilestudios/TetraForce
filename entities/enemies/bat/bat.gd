218acb0946226eb2da3954fb52f3133fe888085d
TheRetroDragon <andrew@retro-dragon.com>
2021-06-13 19:34:33 -0600

    cucukin clean up / bat clean up

--- entities/enemies/bat/bat.gd
+++ entities/enemies/bat/bat.gd
@@ -1,7 +1,8 @@
 extends Enemy
 
-var movetimer_length = 30
+var movetimer_length = 70
 var movetimer = 0
+var active = false
 
 onready var detect = $PlayerDetect
 
@@ -9,32 +10,47 @@ func _ready():
 	MAX_HEALTH = 0.5
 	DAMAGE = 0.5
 	health = MAX_HEALTH
-	SPEED = 60
+	SPEED = 30
 
 func _physics_process(delta):
+	position += movedir * SPEED * delta
 	var sees_player = false
+		
 	if !network.is_map_host() || is_dead():
 		return
 	for body in detect.get_overlapping_bodies():
 		if body is Player:
 			sees_player = true
-			if !anim.is_playing() && anim.assigned_animation != "sees_player":
-				anim.play("sees_player")
-				network.peer_call(anim, "play", ["sees_player"])
-	if !sees_player && anim.assigned_animation != "no_player":
-		anim.play("no_player")
-		network.peer_call(anim, "play", ["no_player"])
+			if !anim.is_playing() && anim.assigned_animation != "activate":
+				anim.play("activate")
+				network.peer_call(anim, "play", ["activate"])
+				if movetimer == 0:
+						movetimer = movetimer_length
+			if movetimer > 0:
+				pursue_player()
+
+	if movetimer <= 0 && !sees_player:
+		movedir = Vector2.ZERO
+		if anim.current_animation == "sees_player":
+			anim.play("land")
+			network.peer_call(anim, "play", ["land"])
+			yield(get_tree().create_timer(5), "timeout")
+	
+	if anim.current_animation == "activate":
+		return
+	
+	if anim.current_animation != "sees_player" && movetimer > 0:
+		anim.play("sees_player")
+		network.peer_call(anim, "play", ["sees_player"])
 
 	loop_movement()
-	loop_spritedir()
 	loop_damage()
-	loop_holes()
-
+	
 	if movetimer > 0:
 		movetimer -= 1
-	if movetimer == 10:
-		movedir = Vector2.ZERO
-	if movetimer == 0 && sees_player || is_on_wall():
+	
+func pursue_player():
+	if movetimer > 0 || is_on_wall():
 		var players = get_tree().get_nodes_in_group("player")
 		var shortest_distance = 999999
 		var closest_player = null
@@ -43,4 +59,3 @@ func _physics_process(delta):
 				shortest_distance = position.distance_to(player.position)
 				closest_player = player
 		movedir = Vector2(-1,0).rotated(position.angle_to_point(closest_player.position))
-		movetimer = movetimer_length
--- entities/enemies/cucukin/cucukin.gd
+++ entities/enemies/cucukin/cucukin.gd
@@ -2,7 +2,6 @@ extends Enemy
 
 onready var detect = $PlayerDetect
 
-var received_sync = false
 var active = false
 
 func _ready():
@@ -10,12 +9,10 @@ func _ready():
 	
 func _physics_process(delta):
 	position += movedir * SPEED * delta
-	var sees_player = false
 	if !network.is_map_host() || is_dead():
 		return
 	for body in detect.get_overlapping_bodies():
 		if body is Player || is_dead():
-			sees_player = true
 			if active != true:
 				if network.is_map_host():
 					launch()
@@ -27,11 +24,11 @@ func _physics_process(delta):
 	
 func launch():
 	active = true
-	$AnimationPlayer.play("lift")
-	network.peer_call($AnimationPlayer, "play", ["lift"])
+	anim.play("lift")
+	network.peer_call(anim, "play", ["lift"])
 	yield(get_tree().create_timer(5), "timeout")
-	$AnimationPlayer.play("launch")
-	network.peer_call($AnimationPlayer, "play", ["launch"])
+	anim.play("launch")
+	network.peer_call(anim, "play", ["launch"])
 	if is_in_group("invunerable"):
 				remove_from_group("invunerable")
 				$Hitbox.connect("body_entered", self, "body_entered")
@@ -49,27 +46,10 @@ func body_entered(body):
 	movedir = Vector2(0,0)
 	yield(get_tree().create_timer(0.01), "timeout")
 	add_to_group("invunerable")
-	if !received_sync && !is_network_master():
-		$AnimationPlayer.play("explode")
-		network.peer_call($AnimationPlayer, "play", ["explode"])
-		sfx.play("boom")
-		yield(get_tree().create_timer(0.55), "timeout")
-		queue_free()
-		network.peer_call(self, "queue_free")
-	elif body.get_collision_layer_bit(7) == true:
-		return
-	elif body is Entity && body:
-		$AnimationPlayer.play("explode")
-		network.peer_call($AnimationPlayer, "play", ["explode"])
-		sfx.play("boom")
-		yield(get_tree().create_timer(0.55), "timeout")
-		queue_free()
-		network.peer_call(self, "queue_free")
-	else:
-		$AnimationPlayer.play("explode")
-		network.peer_call($AnimationPlayer, "play", ["explode"])
-		sfx.play("boom")
-		yield(get_tree().create_timer(0.55), "timeout")
-		queue_free()
-		network.peer_call(self, "queue_free")
+	anim.play("explode")
+	network.peer_call(anim, "play", ["explode"])
+	sfx.play("boom")
+	yield(get_tree().create_timer(0.55), "timeout")
+	queue_free()
+	network.peer_call(self, "queue_free")
 	
