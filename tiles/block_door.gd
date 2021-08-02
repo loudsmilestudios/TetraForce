fd435bd3672058410c53c8b899159c883330a63f
TheRetroDragon <andrew@retro-dragon.com>
2021-08-01 19:32:31 -0600

    Door Switch Fully Implemented

--- tiles/block_door.gd
+++ tiles/block_door.gd
@@ -1,75 +1,43 @@
 extends StaticBody2D
 
-export(bool) var enemy_trigger = false
 export(bool) var starts_locked = false
 export(String) var location = "room"
 export(String) var direction = "up"
 export var texture = "dungeon1"
 
 onready var locked = false setget set_locked
-onready var inactive = false setget set_inactive
+
+signal on_button_pressed
+signal no_weight
 
 func _ready():
 	spritedir()
-	get_parent().get_node(location).connect("finished", self, "set_locked", [false])
-	get_parent().get_node(location).connect("started", self, "set_locked", [true])
-	get_parent().get_node(location).connect("check_for_active", self, "check_lock_state")
-	get_parent().get_node(location).connect("check_for_inactive", self, "check_lock_state")
-	get_parent().get_node(location).connect("reset", self, "set_reset")
-	if starts_locked && inactive == false:
-		$AnimationPlayer.play("enemy_locked_" + direction)
-		network.peer_call($AnimationPlayer, "play", ["enemy_locked_" + direction])
-		locked = true
+	get_parent().get_node(location).connect("on_button_pressed", self, "unlock")
+	get_parent().get_node(location).connect("no_weight", self, "lock")
+	set_locked(starts_locked)
 		
 func lock():
-	if !starts_locked:
-		$AnimationPlayer.play("enemy_lock_" + direction)
-		network.peer_call($AnimationPlayer, "play", ["enemy_lock_" + direction])
+	if locked:
+		return
+	if !locked && $AnimationPlayer.current_animation != "lock_" + direction:
+		$AnimationPlayer.play("lock_" + direction)
+		yield($AnimationPlayer, "animation_finished")
 		set_locked(true)
 
 func unlock():
-	if starts_locked && inactive:
-		$AnimationPlayer.play("enemy_unlock_" + direction)
-		network.peer_call($AnimationPlayer, "play", ["enemy_unlocked_" + direction])
-	else:
-		$AnimationPlayer.play("enemy_unlock_" + direction)
-		network.peer_call($AnimationPlayer, "play", ["enemy_unlock_" + direction])
+	if !locked:
+		return
+	if locked && $AnimationPlayer.current_animation != "unlock_" + direction:
+		$AnimationPlayer.play("unlock_" + direction)
+		yield($AnimationPlayer, "animation_finished")
 		set_locked(false)
-		set_inactive(true)
-
-func check_lock_state():
-	if locked == true:
-		$AnimationPlayer.play("enemy_locked_" + direction)
-		network.peer_call($AnimationPlayer, "play", ["enemy_locked_" + direction])
-	if locked == false:
-		$AnimationPlayer.play("enemy_unlocked_" + direction)
-		network.peer_call($AnimationPlayer, "play", ["enemy_unlocked_" + direction])
-	
-func set_reset():
-	lock()
-	if !starts_locked:
-		locked = false
-		if network.is_map_host():
-			network.peer_call(self, "unlock")
-		unlock()
 
 func set_locked(value):
-	if locked == value:
-		return
-	if network.is_map_host():
-		network.peer_call(self, "set_locked", [value])
 	locked = value
 	if !locked:
-		unlock()
+		$AnimationPlayer.play("unlocked_" + direction)
 	else:
-		lock()
-		
-func set_inactive(value):
-	if inactive == value:
-		return
-	if network.is_map_host():
-		network.peer_call(self, "set_inactive", [value])
-	inactive = value
+		$AnimationPlayer.play("locked_" + direction)
 		
 func spritedir():
 	if direction == "up":
