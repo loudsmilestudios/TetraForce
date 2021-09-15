a7998fb5c1bfad62998e14eb3fe80ec554ec3497
Joseph Manley <j@cloudsumu.com>
2021-09-14 22:14:15 -0400

    PvP Toggle added to options & Volume Bugfix (#310)
    * Bugfix: Use math function to convert volume values
    
    * Make PvP toggle fit theme better by changing from checkbox to button
    
    * Sound slider range from 0 to 100
    
    * Bugfixes to make things work more as expected
    
    * PvP Toggle Bugfix
    
    * Remove bad button

--- ui/options/sound.gd
+++ ui/options/sound.gd
@@ -5,9 +5,9 @@ onready var master_slider : Slider = $VBoxContainer/Master/Slider
 func _ready():
 	global.connect("options_loaded", self, "update_options")
 	
-	master_slider.value = db_to_percent(0)
+	master_slider.value = 50
+	on_master_slider_change(master_slider.value)
 	master_slider.connect("value_changed", self, "on_master_slider_change")
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 0)
 
 func update_options():
 	if not "sound" in global.options:
@@ -17,10 +17,10 @@ func update_options():
 		on_master_slider_change(master_slider.value)
 
 func db_to_percent(db : float) -> float:
-	return ((db + 6) / 86) * 100
+	return db2linear(db)
 
 func percent_to_db(pct : float) -> float:
-	return pct / 100 * 86 - 6
+	return linear2db(pct / 100)
 
 func on_master_slider_change(new_value):
 	if not "sound" in global.options:
