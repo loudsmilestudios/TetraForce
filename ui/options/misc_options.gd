ba95cc98eaa350abec93db06e4fb2e057e8fe2ee
Manley <j@cloudsumu.com>
2021-10-24 21:05:25 -0400

    Options Improvements

--- ui/options/misc_options.gd
+++ ui/options/misc_options.gd
@@ -1,14 +1,18 @@
 extends Control
 
-onready var censor_button = $CensorButton
+onready var censor_button = $VBoxContainer/CensorToggle
 
 func _ready():
 	global.connect("options_loaded", self, "update_options")
-	censor_button.connect("button_down", self, "toggle_censor")
+	censor_button.connect("pressed", self, "toggle_censor")
 
 func toggle_censor():
-	global.options.misc.censor = !global.options.misc.censor
-	update_options()
+	if censor_button.pressed:
+		global.options.misc.censor = true
+		sfx.play("sword0")
+	else:
+		global.options.misc.censor = false
+		sfx.play("swordcharge")
 
 func update_options():
 	if not "misc" in global.options:
@@ -17,6 +21,6 @@ func update_options():
 		}
 	
 	if global.options.misc.censor:
-		censor_button.text = "Disable Text Filter"
+		censor_button.pressed = true
 	else:
-		censor_button.text = "Enable Text Filter"
+		censor_button.pressed = false
