2be14bd7ee099b00b9a036f50bea1d2b9a86eafa
Joseph B. Manley <j@cloudsumu.com>
2022-01-08 20:52:08 -0500

    Update network_object.gd

--- engine/network_object.gd
+++ engine/network_object.gd
@@ -11,7 +11,7 @@ onready var game = get_parent()
 func _ready():
 	if game.has_method("get_game"):
 		game = game.get_game(self)
-	else not game.has_method("is_game"):
+	elif not game.has_method("is_game"):
 		game = game.get_parent()
 
 	game.connect("player_entered", self, "player_entered")
