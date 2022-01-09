b6b07ba9a77d7d28a58e9c5e9e4995f91eda9cde
Joseph B. Manley <j@cloudsumu.com>
2022-01-08 20:28:48 -0500

    Update network_object.gd

--- engine/network_object.gd
+++ engine/network_object.gd
@@ -6,16 +6,16 @@ export(bool) var sync_creation = false
 export(Dictionary) var update_properties = {}
 export(Dictionary) var enter_properties = {}
 
+onready var parent = get_parent()
+
 func _ready():
-	var parent = get_parent()
-	if parent.has_method("get_game"):
-		parent.get_game(self).connect("player_entered", self, "player_entered")
-	else:
-		parent.get_parent().connect("player_entered", self, "player_entered")
+	if not parent.has_method("get_game):
+	        parent = parent.get_parent()
+	parent.get_game(self).connect("player_entered", self, "player_entered")
 	network.tick.connect("timeout", self, "_tick")
 	if persistent:
-		get_parent().connect("update_persistent_state", self, "update_persistent_state")
-		network.request_persistent_state(get_parent())
+		parent.connect("update_persistent_state", self, "update_persistent_state")
+		network.request_persistent_state(parent)
 
 func _tick():
 	if require_map_host && !network.is_map_host():
@@ -35,27 +35,27 @@ func player_entered(id):
 		# persistent == true
 		return
 	if sync_creation:
-		network.peer_create_id(id, get_parent().filename, get_parent().name, get_parent().get_parent())
+		network.peer_create_id(id, parent.filename, parent.name, parent.get_parent())
 		return
 	update_enter_properties(id)
 
 func update_enter_properties(id):
 	for key in enter_properties.keys():
-		enter_properties[key] = get_parent().get(str(key))
+		enter_properties[key] = parent.get(str(key))
 	network.peer_call_id(id, self, "receive_update", [enter_properties])
 
 func update_sync():
 	for key in update_properties.keys():
-		update_properties[key] = get_parent().get(str(key))
+		update_properties[key] = parent.get(str(key))
 	network.peer_call_unreliable(self, "receive_update", [update_properties])
 
 func update_persistent_state():
 	if !network.is_map_host():
 		return
 	for key in enter_properties.keys():
-		enter_properties[key] = get_parent().get(str(key))
-	network.persistent_set_state(str(get_parent().get_path()), enter_properties)
+		enter_properties[key] = parent.get(str(key))
+	network.persistent_set_state(str(parent.get_path()), enter_properties)
 
 func receive_update(properties = {}):
 	for key in properties.keys():
-		get_parent().set(key, properties[key])
+		parent.set(key, properties[key])
